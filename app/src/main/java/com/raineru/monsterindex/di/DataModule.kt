9b2a253333fa6928f053027b93e658fca7acbcf5
rynel <ranajirune@gmail.com>
2025-01-03 03:14:48 +0800

    Added DataModule to provide dependencies used to fetch data from the database or from the network.

---
+++ app/src/main/java/com/raineru/monsterindex/di/DataModule.kt
@@ -0,0 +1,49 @@
+package com.raineru.monsterindex.di
+
+import com.raineru.monsterindex.PokeApiClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun providePokeApiClient(httpClient: HttpClient): PokeApiClient =
+        PokeApiClient(httpClient)
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient = HttpClient(OkHttp) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
+        }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    encodeDefaults = true
+                    isLenient = true
+                    allowSpecialFloatingPointValues = true
+                    allowStructuredMapKeys = true
+                    prettyPrint = false
+                    useArrayPolymorphism = false
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+}
