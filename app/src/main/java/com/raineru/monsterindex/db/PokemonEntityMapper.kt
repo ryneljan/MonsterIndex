19e28e0e5ffd5ba2f32b77ac52788c365dba4464
rynel <ranajirune@gmail.com>
2025-01-08 02:43:19 +0800

    Added PokemonEntityMapper, used to convert Pokemon from an entity to a domain or vice-versa.

---
+++ app/src/main/java/com/raineru/monsterindex/db/PokemonEntityMapper.kt
@@ -0,0 +1,35 @@
+package com.raineru.monsterindex.db
+
+import com.raineru.monsterindex.data.Pokemon
+import com.raineru.monsterindex.db.entity.PokemonEntity
+
+object PokemonEntityMapper {
+
+    fun asEntity(domain: List<Pokemon>): List<PokemonEntity> {
+        return domain.map {
+            PokemonEntity(
+                page = it.page,
+                name = it.name,
+                url = it.url
+            )
+        }
+    }
+
+    fun asDomain(entity: List<PokemonEntity>): List<Pokemon> {
+        return entity.map {
+            Pokemon(
+                page = it.page,
+                nameField = it.name,
+                url = it.url
+            )
+        }
+    }
+}
+
+fun List<PokemonEntity>.asDomain(): List<Pokemon> {
+    return PokemonEntityMapper.asDomain(this)
+}
+
+fun List<Pokemon>.asEntity(): List<PokemonEntity> {
+    return PokemonEntityMapper.asEntity(this)
+}
