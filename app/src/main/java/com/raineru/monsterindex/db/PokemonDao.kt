6687ee953dca7fe0480bd195fd468a440ba97316
rynel <ranajirune@gmail.com>
2025-01-08 02:37:42 +0800

    Added PokemonDao, used to access functions to save and retrieve Pokemon-related info in the db.

---
+++ app/src/main/java/com/raineru/monsterindex/db/PokemonDao.kt
@@ -0,0 +1,20 @@
+package com.raineru.monsterindex.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.raineru.monsterindex.db.entity.PokemonEntity
+
+@Dao
+interface PokemonDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPokemonList(pokemonList: List<PokemonEntity>)
+
+    @Query("SELECT * FROM PokemonEntity WHERE page = :page")
+    suspend fun getPokemonList(page: Int): List<PokemonEntity>
+
+    @Query("SELECT * FROM PokemonEntity WHERE page <= :page")
+    suspend fun getAllPokemonList(page: Int): List<PokemonEntity>
+}
