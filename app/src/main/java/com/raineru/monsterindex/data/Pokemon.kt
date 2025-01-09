37e743d7dd1ea1d828f6a40a94adc671d2373525
rynel <ranajirune@gmail.com>
2025-01-10 02:47:10 +0800

    Added imageUrl field, used to get the Pokemon's artwork

--- app/src/main/java/com/raineru/monsterindex/data/Pokemon.kt
+++ app/src/main/java/com/raineru/monsterindex/data/Pokemon.kt
@@ -16,4 +16,11 @@ data class Pokemon(
 
     val name: String
         get() = nameField.replaceFirstChar { it.uppercase() }
+
+    val imageUrl: String
+        inline get() {
+            val index = url.split("/".toRegex()).dropLast(1).last()
+            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/" +
+                    "pokemon/other/official-artwork/$index.png"
+        }
 }
