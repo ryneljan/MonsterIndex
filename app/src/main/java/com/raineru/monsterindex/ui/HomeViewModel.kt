04cf4815702af23630b93a5ef23ee6fff3186430
rynel <ranajirune@gmail.com>
2025-01-16 02:39:01 +0800

    Added isLoading field to track when homeRepository is currently fetching a new page of Pokemons.

--- app/src/main/java/com/raineru/monsterindex/ui/HomeViewModel.kt
+++ app/src/main/java/com/raineru/monsterindex/ui/HomeViewModel.kt
@@ -6,9 +6,11 @@ import com.raineru.monsterindex.data.HomeRepository
 import com.raineru.monsterindex.data.Pokemon
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -19,10 +21,16 @@ class HomeViewModel @Inject constructor(
     private val homeRepository: HomeRepository
 ) : ViewModel() {
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
     val pokemonFetchingIndex: MutableStateFlow<Int> = MutableStateFlow(0)
     @OptIn(ExperimentalCoroutinesApi::class)
     val pokemonList: StateFlow<List<Pokemon>> = pokemonFetchingIndex.flatMapLatest { index ->
-        homeRepository.fetchPokemonList(index + 1)
+        _isLoading.update { true }
+        val pokemons: Flow<List<Pokemon>> = homeRepository.fetchPokemonList(index + 1)
+        _isLoading.update { false }
+        pokemons
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
@@ -34,4 +42,10 @@ class HomeViewModel @Inject constructor(
             it + 1
         }
     }
+}
+
+sealed class HomeScreenUiState {
+    data object Loading : HomeScreenUiState()
+    data object Idle : HomeScreenUiState()
+//    data class Error(val message: String?) : HomeScreenUiState()
 }
