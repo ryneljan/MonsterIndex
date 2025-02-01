7942884799147c44908e292373abb10d860b835d
rynel <ranajirune@gmail.com>
2025-02-02 02:03:54 +0800

    Added TypeConverters annotation for StatsResponseConverter and TypeResponseConverter
    Also added function to provide PokemonInfoDao

--- app/src/main/java/com/raineru/monsterindex/db/MonsterIndexDatabase.kt
+++ app/src/main/java/com/raineru/monsterindex/db/MonsterIndexDatabase.kt
@@ -2,14 +2,19 @@ package com.raineru.monsterindex.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.raineru.monsterindex.db.entity.PokemonEntity
+import com.raineru.monsterindex.db.entity.PokemonInfoEntity
 
 @Database(
-    entities = [PokemonEntity::class],
+    entities = [PokemonEntity::class, PokemonInfoEntity::class],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(StatsResponseConverter::class, TypeResponseConverter::class)
 abstract class MonsterIndexDatabase : RoomDatabase() {
 
     abstract fun pokemonDao(): PokemonDao
+
+    abstract fun pokemonInfoDao(): PokemonInfoDao
 }
