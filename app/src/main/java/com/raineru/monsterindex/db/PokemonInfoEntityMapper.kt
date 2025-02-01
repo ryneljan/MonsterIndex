040cdaeaa09428c9dee62a07ec59ab86baca9281
rynel <ranajirune@gmail.com>
2025-02-02 01:48:26 +0800

    Added PokemonInfoEntityMapper to easily convert PokemonInfo to PokemonInfoEntity and vice-versa

---
+++ app/src/main/java/com/raineru/monsterindex/db/PokemonInfoEntityMapper.kt
@@ -0,0 +1,31 @@
+package com.raineru.monsterindex.db
+
+import com.raineru.monsterindex.db.entity.PokemonInfoEntity
+import com.raineru.monsterindex.model.PokemonInfo
+
+object PokemonInfoEntityMapper {
+
+    fun asEntity(domain: PokemonInfo): PokemonInfoEntity {
+        return PokemonInfoEntity(
+            id = domain.id,
+            name = domain.name,
+            height = domain.height,
+            weight = domain.weight,
+            baseExperience = domain.baseExperience,
+            types = domain.types,
+            stats = domain.stats
+        )
+    }
+
+    fun asDomain(entity: PokemonInfoEntity): PokemonInfo {
+        return PokemonInfo(
+            id = entity.id,
+            name = entity.name,
+            height = entity.height,
+            weight = entity.weight,
+            baseExperience = entity.baseExperience,
+            types = entity.types,
+            stats = entity.stats
+        )
+    }
+}
