a73c17ebd32fca7a82a9e07598e6c17d79172a91
rynel <ranajirune@gmail.com>
2025-02-02 01:59:38 +0800

    Added StatsReponse and TypeResponse type converters when constructing the db
    Also added functions to provide StatsReponse and TypeResponse dependencies

--- app/src/main/java/com/raineru/monsterindex/di/DatabaseModule.kt
+++ app/src/main/java/com/raineru/monsterindex/di/DatabaseModule.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.room.Room
 import com.raineru.monsterindex.db.MonsterIndexDatabase
 import com.raineru.monsterindex.db.PokemonDao
+import com.raineru.monsterindex.db.StatsResponseConverter
+import com.raineru.monsterindex.db.TypeResponseConverter
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
@@ -21,12 +24,32 @@ object DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideDatabase(@ApplicationContext context: Context): MonsterIndexDatabase =
-        Room.databaseBuilder(
-            context,
-            MonsterIndexDatabase::class.java,
-            "monster_index_db"
-        )
+    fun provideStatsResponseConverter(json: Json): StatsResponseConverter {
+        return StatsResponseConverter(json)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTypeResponseConverter(json: Json): TypeResponseConverter {
+        return TypeResponseConverter(json)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context,
+        statsResponseConverter: StatsResponseConverter,
+        typeResponseConverter: TypeResponseConverter
+    ): MonsterIndexDatabase {
+        return Room
+            .databaseBuilder(
+                context,
+                MonsterIndexDatabase::class.java,
+                "monster_index_db"
+            )
             .fallbackToDestructiveMigration()
+            .addTypeConverter(statsResponseConverter)
+            .addTypeConverter(typeResponseConverter)
             .build()
+    }
 }
