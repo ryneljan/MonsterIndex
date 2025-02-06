52d53ce0c9fd3ee2082d54f9014f4aa796ad7f8b
rynel <ranajirune@gmail.com>
2025-02-06 20:54:43 +0800

    Added fields to return values of a pokemon's base stat

--- app/src/main/java/com/raineru/monsterindex/model/PokemonInfo.kt
+++ app/src/main/java/com/raineru/monsterindex/model/PokemonInfo.kt
@@ -2,6 +2,7 @@ package com.raineru.monsterindex.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlin.random.Random
 
 @Serializable
 data class PokemonInfo(
@@ -19,6 +20,37 @@ data class PokemonInfo(
     @SerialName(value = "types")
     val types: List<TypeResponse>
 ) {
+    val hp: Int by lazy {
+        stats.firstOrNull { it.stat.name == "hp" }?.baseStat ?: Random.nextInt(MAX_BASE_STAT)
+    }
+
+    val attack: Int by lazy {
+        stats.firstOrNull { it.stat.name == "attack" }?.baseStat ?: Random.nextInt(MAX_BASE_STAT)
+    }
+
+    val defense: Int by lazy {
+        stats.firstOrNull { it.stat.name == "defense" }?.baseStat ?: Random.nextInt(MAX_BASE_STAT)
+    }
+
+    val specialAttack: Int by lazy {
+        stats.firstOrNull { it.stat.name == "special-attack" }?.baseStat ?: Random.nextInt(MAX_BASE_STAT)
+    }
+
+    val specialDefense: Int by lazy {
+        stats.firstOrNull { it.stat.name == "special-defense" }?.baseStat ?: Random.nextInt(MAX_BASE_STAT)
+    }
+
+    val speed: Int by lazy {
+        stats.firstOrNull { it.stat.name == "speed" }?.baseStat ?: Random.nextInt(MAX_BASE_STAT)
+    }
+
+    fun getHpString(): String = " $hp/$MAX_BASE_STAT"
+    fun getAttackString(): String = " $attack/$MAX_BASE_STAT"
+    fun getDefenseString(): String = " $defense/$MAX_BASE_STAT"
+    fun getSpecialAttackString(): String = " $specialAttack/$MAX_BASE_STAT"
+    fun getSpecialDefenseString(): String = " $specialDefense/$MAX_BASE_STAT"
+    fun getSpeedString(): String = " $speed/$MAX_BASE_STAT"
+
     @Serializable
     data class StatsResponse(
         @SerialName(value = "base_stat") val baseStat: Int,
@@ -41,4 +73,14 @@ data class PokemonInfo(
     data class Type(
         @SerialName(value = "name") val name: String,
     )
+
+    val weightInKilogram: Double
+        get() = weight.toDouble() / 10.0
+
+    val heightInMeter: Double
+        get() = height.toDouble() / 10.0
+
+    companion object {
+        const val MAX_BASE_STAT = 255
+    }
 }
