d916dfb20b8397b39c3113fb343872585151867d
rynel <ranajirune@gmail.com>
2025-02-19 23:55:41 +0800

    Added functions to support paging library

--- app/src/main/java/com/raineru/monsterindex/db/PokemonDao.kt
+++ app/src/main/java/com/raineru/monsterindex/db/PokemonDao.kt
@@ -1,5 +1,6 @@
 package com.raineru.monsterindex.db
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -17,4 +18,15 @@ interface PokemonDao {
 
     @Query("SELECT * FROM pokemons WHERE page <= :page ORDER BY id ASC")
     suspend fun getAllPokemonList(page: Int): List<PokemonEntity>
+
+    @Query("DELETE FROM pokemons")
+    suspend fun clearAllPokemons()
+
+    // Used by paging library
+    @Query("SELECT * FROM pokemons ORDER BY id ASC")
+    fun pagingSource(): PagingSource<Int, PokemonEntity>
+
+    // Return the last id of the last fetched Pokemon from the API
+    @Query("SELECT * FROM pokemons ORDER BY id DESC LIMIT 1")
+    suspend fun getPokemonWithGreatestId(): PokemonEntity?
 }
