b8d8f969206eb9a220147a9b2f14b16da723d4ca
rynel <ranajirune@gmail.com>
2025-02-26 01:35:29 +0800

    Added CollapsingAppBarNestedScrollConnection, used to offset an app bar when a list is scrolled

---
+++ app/src/main/java/com/raineru/monsterindex/CollapsingAppBarNestedScrollConnection.kt
@@ -0,0 +1,30 @@
+package com.raineru.monsterindex
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+
+@OptIn(ExperimentalMaterial3Api::class)
+val AppBarHeight = TopAppBarDefaults.TopAppBarExpandedHeight
+
+class CollapsingAppBarNestedScrollConnection(
+    val appBarMaxHeight: Int
+) : NestedScrollConnection {
+
+    var appBarOffset: Int by mutableIntStateOf(0)
+        private set
+
+    override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+        val delta = available.y.toInt()
+        val newOffset = appBarOffset + delta
+        val previousOffset = appBarOffset
+        appBarOffset = newOffset.coerceIn(-appBarMaxHeight, 0)
+        val consumed = appBarOffset - previousOffset
+        return Offset(0f, consumed.toFloat())
+    }
+}
