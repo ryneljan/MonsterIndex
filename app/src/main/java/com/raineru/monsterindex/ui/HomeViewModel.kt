619823f3d3072d5664df1341de7aee9a25fa775d
rynel <ranajirune@gmail.com>
2025-02-26 01:29:14 +0800

    - Added textFieldState, used to store search query in the HomeScreen - Added Flow.combine to filter PagingData upon the user entering new search query

--- app/src/main/java/com/raineru/monsterindex/ui/HomeViewModel.kt
+++ app/src/main/java/com/raineru/monsterindex/ui/HomeViewModel.kt
@@ -1,11 +1,17 @@
 package com.raineru.monsterindex.ui
 
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.cachedIn
+import androidx.paging.filter
 import com.raineru.monsterindex.db.entity.PokemonEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,7 +19,18 @@ class HomeViewModel @Inject constructor(
     pager: Pager<Int, PokemonEntity>
 ) : ViewModel() {
 
+    val textFieldState = TextFieldState()
+
+    private val textFieldStateFlow = snapshotFlow { textFieldState.text }
+
+    @OptIn(FlowPreview::class)
     val pokemonListViaPaging = pager
         .flow
+        .cachedIn(viewModelScope)
+        .combine(textFieldStateFlow.debounce(500)) { pagingData, query ->
+            pagingData.filter {
+                it.name.contains(query, ignoreCase = true)
+            }
+        }
         .cachedIn(viewModelScope)
 }
